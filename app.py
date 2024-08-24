d745d8bcf582d93376e94e40fa2bbeeb9b52159a
Ryan Baker <rbaker@xtier.com>
2024-08-24 15:03:33 -0400

    buggy field

--- app.py
+++ app.py
@@ -1,21 +1,51 @@
 import customtkinter as tk
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import numpy as np
 
-tk.set_appearance_mode("dark")
-tk.set_default_color_theme("dark-blue")
+def plotSlopeField(event, bounds=((-3, 3), (-3, 3)), n=60):
+	equation = equation_field.get()
+	x_coords = np.linspace(bounds[0][0], bounds[0][1], n)
+	y_coords = np.linspace(bounds[1][0], bounds[1][1], n)
 
-root = tk.CTk()
-root.geometry("500x350")
+	ax.clear()
+	L = 2.0 / n
+	for x in x_coords:
+		for y in y_coords:
+			m = eval(equation, {'x': x, 'y': y})
+			theta = np.arctan(x)
+			x0, x1, y0, y1 = x-L*np.cos(theta), x+L*np.cos(theta), y-L*np.sin(theta), y+L*np.sin(theta)
+			ax.plot([x0, x1], [y0, y1], color="black", marker='', linewidth=0.5)
+	
+	canvas.draw()
 
-frame = tk.CTkFrame(master=root)
-frame.pack(pady=20, padx=60, fill="both", expand=True)
+if __name__ == "__main__":
+	tk.set_appearance_mode("dark")
+	tk.set_default_color_theme("dark-blue")
 
-label = tk.CTkLabel(master=frame, text="Slope Field Plotter")
-label.pack(pady=12, padx=10)
+	root = tk.CTk()
+	root.geometry("600x450")
+	root.title("Slope Field Plotter")
 
-entry1 = tk.CTkEntry(master=frame, placeholder_text="Username")
-entry1.pack(pady=12, padx=10)
+	plot_frame = tk.CTkFrame(master=root)
+	plot_frame.pack(pady=10, padx=20, fill="both", expand=True)
 
-entry2 = tk.CTkEntry(master=frame, placeholder_text="Password", show='*')
-entry2.pack(pady=12, padx=10)
+	fig = Figure(figsize=(5, 3.5), dpi=100)
+	ax = fig.add_subplot(111)
 
-root.mainloop()
+	x = np.linspace(0, 10, 100)
+	y = np.sin(x)
+	ax.plot(x, y)
+
+	canvas = FigureCanvasTkAgg(fig, master=plot_frame)
+	canvas.draw()
+	canvas.get_tk_widget().pack(fill="both", expand=True)
+
+	input_frame = tk.CTkFrame(master=root)
+	input_frame.pack(pady=10, padx=20, fill='x')
+
+	equation_field = tk.CTkEntry(master=input_frame, placeholder_text="dy/dx = ")
+	equation_field.pack(pady=12, padx=10, fill='x')
+	equation_field.bind("<Return>", plotSlopeField)
+
+	root.mainloop()
